c62aaf8ed064b25587c95ff995e5a0e44090ec06
mohsin12225 <mohsin.alaum10@gmail.com>
2026-03-22 21:34:02 +0500

    Delete src directory

--- src/store/index.ts
+++
@@ -1,5 +0,0 @@
-export { useThemeStore } from './useThemeStore';
-export { useProjectStore } from './useProjectStore';
-export { useProcessingStore } from './useProcessingStore';
-export { useClipStore } from './useClipStore';
-export { useUIStore } from './useUIStore';
